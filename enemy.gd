4d09cf0ec6ebd35758ce04786e55db46df12d9b9
Sayyid aja <muhammad.sayyid751@smk.belajar.id>
2026-05-26 21:52:02 +0700

    menambahkan:enemy/musuh

---
+++ enemy.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+const speed : float  = 0.9
+
+
+func _physics_process(delta: float) -> void:
+	$AnimatedSprite2D.play("green_enemy")
+	if !is_on_floor():
+		velocity += get_gravity() 
+	
+	velocity.x += speed if animated_sprite_2d.flip_h == false else -speed
+	flip()
+	move_and_slide()
+
+func flip():
+	if ray_cast_2d.is_colliding() == false or is_on_wall() == true:
+		velocity.x = 0
+		animated_sprite_2d.flip_h = !animated_sprite_2d.flip_h
+		ray_cast_2d.position.x = -ray_cast_2d.position.x
+
+
+func _on_hurt_box_body_entered(body: Node2D) -> void:
+	if body.name == "player":
+		body.respawn()
+
+
+func _on_hit_enemy_body_entered(body: Node2D) -> void:
+	if body.name == "player":
+		queue_free()
