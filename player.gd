8fa42f19fd4e4d7152c476f0cea5652c884d9ba1
ammarsetiawan <ammarsetiawan970@gmail.com>
2026-06-12 21:55:22 +0700

    Refactor UI components
    Co-authored-by: ammarsetiawan <ammarsetiawan970@gmail.com>

--- player.gd
+++ player.gd
@@ -6,7 +6,7 @@ const JUMP_VELOCITY = -400.0
 const MAX_JUMPS = 2
 
 var jumps_left := MAX_JUMPS
-var is_double_jumping := false  # 🔥 Tambahin ini buat nandain double jump
+var is_double_jumping := false  #  ini buat nandain double jump
 
 func _ready() -> void:
 	$effect_respawn.hide()
@@ -14,8 +14,8 @@ func _ready() -> void:
 func respawn():	
 	$effect_respawn.show()
 	$AnimatedSprite2D.hide()
-	velocity = Vector2.ZERO  # Stop semua gerakan biar nggak nyangkut
-	global_position = Vector2(88.0, 348.0)  # Ganti koordinat ini sesuai posisi spawn lu
+	velocity = Vector2.ZERO  
+	global_position = Vector2(88.0, 348.0) 
 	print("✅ Respawn berhasil!")
 	$effect_respawn.play("efek_respawn")
 	await $effect_respawn.animation_finished
@@ -28,13 +28,13 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# 2. LOGIKA JUMP + TANDAIN DOUBLE JUMP
+	# 2. LOGIKA JUMP + DOUBLE JUMP
 	if Input.is_action_just_pressed("ui_accept"):
 		if is_on_floor():
 			jumps_left = MAX_JUMPS
-			is_double_jumping = false          # Reset pas nyentuh tanah
-		elif jumps_left < MAX_JUMPS:           # Artinya ini lompatan ke-2/3/dst
-			is_double_jumping = true           # 🔥 Tandain double jump
+			is_double_jumping = false          
+		elif jumps_left < MAX_JUMPS:          
+			is_double_jumping = true          
 			
 		if jumps_left > 0:
 			velocity.y = JUMP_VELOCITY
@@ -47,7 +47,6 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
-		#jump_effect
 	if  velocity.x < 0 || velocity.x > 0 || !is_on_floor():
 		partikel.emitting = true
 	elif velocity.x == 0:
@@ -56,27 +55,25 @@ func _physics_process(delta: float) -> void:
 	
 
 
-	# 4. GERAKKAN KARAKTER
+
 	move_and_slide()
 
-	# 🔥 5. UPDATE ANIMASI (WAJIB SETELAH move_and_slide)
 	_update_animasi()
 
-# 👇🏼 INI TEMPAT ANIMASI DOUBLE JUMP NYA
+
 func _update_animasi():
-	$AnimatedSprite2D.flip_h = velocity.x < 0  # Flip ikut arah gerak
+	$AnimatedSprite2D.flip_h = velocity.x < 0  
 	
 	if is_on_floor():
-		is_double_jumping = false  # Pastiin reset pas mendarat
+		is_double_jumping = false 
 		if abs(velocity.x) > 1.0:
 			$AnimatedSprite2D.play("jalan")
 		else:
 			$AnimatedSprite2D.play("diam")
 	else:
-		# 🔥 CEK DOUBLE JUMP DULUAN
 		if is_double_jumping:
-			$AnimatedSprite2D.play("double_jump")  # ← Ganti nama sesuai animasi kamu
+			$AnimatedSprite2D.play("double_jump")  
 		elif velocity.y > 0 :
 			$AnimatedSprite2D.play("jatuh")
 		else:
-			$AnimatedSprite2D.play("lompat")       # Animasi lompatan biasa
+			$AnimatedSprite2D.play("lompat")    
